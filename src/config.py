081b83c57f70ee142ad0993a677dd54fa8469566
Oluwatobi Adefisan <al426468@uji.es>
2025-05-17 14:55:00 +0200

    Update image directory and config.py

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 # Configuration file for project paths and constants
 
 # Paths
-RAW_DATA_PATH = "./data/raw/"
-PROCESSED_DATA_PATH = "./data/processed/"
+RAW_DATA_PATH = "../data/raw/"
+PROCESSED_DATA_PATH = "../data/processed/"
 GEOJSON_PATH = "../data/raw/georef-spain-provincia.geojson"
-IMAGES_PATH = "./data/images/"
+IMAGES_PATH = "../data/images/"
 
 # Map settings
 CASTELLON_CENTER = [39.9860347, -0.0377354]
