3ec284def7851105361d1d14873913cdfa533a32
Oluwatobi Adefisan <al426468@uji.es>
2025-05-17 16:20:13 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
 # Makes the src folder a Python package.
+# This allows us to import modules from this folder in other scripts.
+# This is necessary for the package to be recognized by Python.
+# The __init__.py file can be empty, or it can contain initialization code for the package.
